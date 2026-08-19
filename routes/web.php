86f190b073ad48adb5a3313caa7c30c4de8ef9e9
Jonathan Attram <codinghub242@gmail.com>
2026-08-19 12:38:28 +0000

    up

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+/* ----------------------------- Admin auth ----------------------------- */
+Route::get('/admin/login', [AuthController::class, 'showAdminLogin'])->name('admin.login');
+Route::post('/admin/login', [AuthController::class, 'adminLogin'])->name('admin.login.post');
+Route::post('/admin/logout', [AuthController::class, 'adminLogout'])->name('admin.logout');
+
+/* ----------------------------- Client auth ----------------------------- */
+Route::get('/tracker/login', [AuthController::class, 'showClientLogin'])->name('client.login');
+Route::post('/tracker/login', [AuthController::class, 'clientLogin'])->name('client.login.post');
+Route::post('/tracker/logout', [AuthController::class, 'clientLogout'])->name('client.logout');
